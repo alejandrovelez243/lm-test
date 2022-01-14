7714d7940584127aa0530764ea0b17f8ff070bd6
Alejandro <alejandro-243@hotmail.com>
2022-01-14 18:28:04 -0500

    Start test

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from zip_code_app import create_app, db
+import unittest
 
 app = create_app()
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,69 +1,77 @@
+from flask_testing import TestCase
+from flask import current_app, url_for
 import pytest
-from flask import g
-from flask import session
+
+from app import app
 
 from zip_code_app.db import get_db
 
+class MainTest(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
 
-def test_register(client, app):
-    # test that viewing the page renders without template errors
-    assert client.get("/auth/register").status_code == 200
+        return app
 
-    # test that successful registration redirects to the login page
-    response = client.post("/auth/register", data={"username": "a", "password": "a"})
-    assert "http://localhost/auth/login" == response.headers["Location"]
+    def test_register(client, app):
+        # test that viewing the page renders without template errors
+        assert client.get("/auth/register").status_code == 200
 
-    # test that the user was inserted into the database
-    with app.app_context():
-        assert (
-            get_db().execute("SELECT * FROM user WHERE username = 'a'").fetchone()
-            is not None
-        )
+        # test that successful registration redirects to the login page
+        response = client.post("/auth/register", data={"username": "a", "password": "a"})
+        assert "http://localhost/auth/login" == response.headers["Location"]
+
+        # test that the user was inserted into the database
+        with app.app_context():
+            assert (
+                get_db().execute("SELECT * FROM user WHERE username = 'a'").fetchone()
+                is not None
+            )
 
 
-@pytest.mark.parametrize(
-    ("username", "password", "message"),
-    (
-        ("", "", b"Username is required."),
-        ("a", "", b"Password is required."),
-        ("test", "test", b"already registered"),
-    ),
-)
-def test_register_validate_input(client, username, password, message):
-    response = client.post(
-        "/auth/register", data={"username": username, "password": password}
+    @pytest.mark.parametrize(
+        ("username", "password", "message"),
+        (
+            ("", "", b"Username is required."),
+            ("a", "", b"Password is required."),
+            ("test", "test", b"already registered"),
+        ),
     )
-    assert message in response.data
+    def test_register_validate_input(client, username, password, message):
+        response = client.post(
+            "/auth/register", data={"username": username, "password": password}
+        )
+        assert message in response.data
 
 
-def test_login(client, auth):
-    # test that viewing the page renders without template errors
-    assert client.get("/auth/login").status_code == 200
+    def test_login(client, auth):
+        # test that viewing the page renders without template errors
+        assert client.get("/auth/login").status_code == 200
 
-    # test that successful login redirects to the index page
-    response = auth.login()
-    assert response.headers["Location"] == "http://localhost/"
+        # test that successful login redirects to the index page
+        response = auth.login()
+        assert response.headers["Location"] == "http://localhost/"
 
-    # login request set the user_id in the session
-    # check that the user is loaded from the session
-    with client:
-        client.get("/")
-        assert session["user_id"] == 1
-        assert g.user["username"] == "test"
-
-
-@pytest.mark.parametrize(
-    ("username", "password", "message"),
-    (("a", "test", b"Incorrect username."), ("test", "a", b"Incorrect password.")),
-)
-def test_login_validate_input(auth, username, password, message):
-    response = auth.login(username, password)
-    assert message in response.data
+        # login request set the user_id in the session
+        # check that the user is loaded from the session
+        with client:
+            client.get("/")
+            assert session["user_id"] == 1
+            assert g.user["username"] == "test"
 
 
-def test_logout(client, auth):
-    auth.login()
+    @pytest.mark.parametrize(
+        ("username", "password", "message"),
+        (("a", "test", b"Incorrect username."), ("test", "a", b"Incorrect password.")),
+    )
+    def test_login_validate_input(auth, username, password, message):
+        response = auth.login(username, password)
+        assert message in response.data
 
-    with client:
-        auth.logout()
-        assert "user_id" not in session
+
+    def test_logout(client, auth):
+        auth.login()
+
+        with client:
+            auth.logout()
+            assert "user_id" not in session
